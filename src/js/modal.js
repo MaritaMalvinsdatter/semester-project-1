07bfd48b8164bdfc1b4b9b53b4585cb81e527df2
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-02-23 15:45:10 +0100

    new listing modal created

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,31 @@
-// const myModal = document.getElementById('myModal')
+
+// const myModal = document.getElementById('listingModal')
 // const myInput = document.getElementById('myInput')
 
 // myModal.addEventListener('shown.bs.modal', () => {
 //   myInput.focus()
 // })
+
+
+
+
+// const modal = document.querySelector("#listingModal");
+
+// // Add an event listener to detect when the modal is shown
+// modal.addEventListener("shown.bs.modal", function () {
+//   // Get the date input element inside the modal
+//   const dateInput = modal.querySelector("#today");
+
+//   // Set the date input value to today's date
+//   dateInput.valueAsDate = new Date();
+// });
+
+const dateInput = document.querySelector("#listingModal #today");
+
+// Use setTimeout to delay the execution of the code by 100 milliseconds
+setTimeout(function() {
+  // Set the date input value to today's date
+  dateInput.valueAsDate = new Date();
+}, 100);
+
 
