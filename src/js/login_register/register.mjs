c2d169ea9f572e14bbc1ff6bf91c0b20c267846e
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-02-24 15:10:08 +0100

    yeah nah

--- src/js/login_register/register.mjs
+++ src/js/login_register/register.mjs
@@ -3,14 +3,27 @@ import { save } from "../api/tokenStorage.mjs";
 
 const action = "/auth/register";
 const method = "post";
-
+const userAlert = document.querySelector("#error-alert");
 
 async function register(user) {
     const registerURL = API_AUCTION_URL + action;
     const body = JSON.stringify(user);
     const response = await fetch(registerURL, { headers: { "Content-Type": "application/json" }, method, body });
+    const { accessToken, ...profile } = await response.json();
     console.log(response);
-    return await response.json();
+    console.log(accessToken);
+    console.log(profile);
+
+    if (profile && accessToken) {
+        save('accessToken', accessToken);
+        save('profile', JSON.stringify(profile));
+        window.location.replace('/index.html');
+    } 
+    // else {
+    //     userAlert.classList.add("alert-warning");
+    //     userAlert.innerHTML += `Something went wrong, please try again`;
+    // }
+    return response;
 }
 
 export function setRegisterFormListener() {
@@ -27,10 +40,8 @@ export function setRegisterFormListener() {
 
             console.log(user);
             
-            const registeredUser = await register(user);
-
-            // Set the new registered user into local storage
-            save('profile', registeredUser);
+            register(user);
+            
         });
     }
 }
