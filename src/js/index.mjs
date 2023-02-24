b727573cf160525a8ef8f71d077b5d44d46e3aa7
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-02-24 08:49:42 +0100

    paths updated

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -4,23 +4,43 @@ import { getListings } from "./listings/getLists.mjs";
 import { getOneListing } from "./listings/getLists.mjs";
 import { setNewListingListener } from "./listings/newListing.mjs";
 import * as paths from "./api/constants.mjs";
-import { logOutEvent } from "./login_register/logout.mjs";
+import { setLogoutListener } from "./login_register/logout.mjs";
 
 // console.log(paths.API_SELLER_URL)
 
 const path = location.pathname
 
-if (path === "/login/login.html") {
-    setLoginFormListener()
-} else if (path === "/register/index.html") {
-    setRegisterFormListener()
-} else if (path === "/index.html") {
-    getListings()
-    logOutEvent()
-    setNewListingListener()
-} else if (path === '/listingItem/index.html') {
-    getOneListing()
-} 
+// if (path === "/login/login.html") {
+//     setLoginFormListener()
+// } else if (path === "/register/index.html") {
+//     setRegisterFormListener()
+// } else if (path === "/index.html") {
+//     getListings()
+//     logOutEvent()
+//     setNewListingListener()
+// } else if (path === '/listingItem/index.html') {
+//     getOneListing()
+// } 
+
+switch (path) {
+    case "/":
+    case "/index.html":
+        getListings();
+        setLogoutListener();
+        setNewListingListener();
+        break;
+    case "/login/":
+    case "/login/index.html":
+        setLoginFormListener();
+        break;
+    case "/register/":
+    case "/register/index.html":
+        setRegisterFormListener();
+        break; 
+    case "/listingItem/index.html":
+        getOneListing();
+        break;       
+}
 
 
 
