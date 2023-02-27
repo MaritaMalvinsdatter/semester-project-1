b6d5466462d8e7d8fae5ef0630275a8c861dcf75
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-02-27 16:27:27 +0100

    updated url so only active listings are showing

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -3,12 +3,12 @@ export const API_CONST = '/api/v1';
 export const API_AUCTION_CONST = '/auction';
 export const API_LISTINGS_CONST = '/listings';
 export const API_SELLER = '_seller=true&_bids=true';
-export const DESC_ORDER = 'sort=created&sortOrder=desc';
+export const DESC_ORDER = 'sort=created&sortOrder=desc&_active=true';
 export const API_AUCTION_URL = `${API_HOST_URL}${API_CONST}${API_AUCTION_CONST}`;
 export const API_LISTINGS_URL = `${API_AUCTION_URL}${API_LISTINGS_CONST}`;
 export const API_SELLER_URL = `${API_LISTINGS_URL}/?${API_SELLER}`;
 export const API_DESC_URL = `${API_LISTINGS_URL}/?${DESC_ORDER}&${API_SELLER}`
 
-// console.log(API_DESC_URL)
+console.log(API_DESC_URL)
 
 
