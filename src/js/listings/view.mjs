aaf9996cb164d5bc5dddf9ca867f8b7498fb96ea
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-02-27 15:48:52 +0100

    user can place bid

--- src/js/listings/view.mjs
+++ src/js/listings/view.mjs
@@ -2,6 +2,8 @@
 // View Full List of Auction Items
 
 import { isLoggedIn } from "../api/helpers.mjs";
+import { API_LISTINGS_URL } from "../api/constants.mjs";
+import { tokenFetch } from "../api/tokenFetch.mjs";
 
 export function listTemplate(listData) {
     const list = document.createElement("div");
@@ -34,7 +36,7 @@ export function listTemplate(listData) {
     <img src="${listData.seller.avatar}" width="50" height="60">`;
     list.append(seller);
 
-      const profileName = JSON.parse(window.localStorage.getItem('profile'))
+      const profileInfo = JSON.parse(window.localStorage.getItem('profile'))
       
 
       if (isLoggedIn) {
@@ -43,7 +45,7 @@ export function listTemplate(listData) {
         btn.innerHTML =`<a href="listingItem/index.html?id=${listData.id}" class="text-muted">Place Bid</a>`;
         list.append(btn);
 
-        if (profileName.name === listData.seller.name) {
+        if (profileInfo.name === listData.seller.name) {
           btn.remove();
           const editBtn = document.createElement("button");
           editBtn.classList.add("mb-2")
@@ -55,78 +57,109 @@ export function listTemplate(listData) {
     return list;
 }
 
-// Auction Item Specifics 
+
+// Auction Listing Specifics 
 
 export function listSpecificTemplate(listData) {
-    const list = document.createElement("div");
-    list.classList.add("mainlist", "sm-md-w-100", "lg-w-50", "container-fluid", "d-flex", "flex-column", "border", "border-primary");
-    list.innerHTML = `<h2 class="text-center mt-5">${listData.title}</h2> <br> <p>${listData.description}</p>`;
-    
-    if (listData.media) {
-        if (Array.isArray(listData.media)) {
-          const imgContainer = document.createElement("div");
-          imgContainer.classList.add("d-flex", "flex-column", "justify-content-center", "align-items-center");
-          for (const imageUrl of listData.media) {
-            const img = document.createElement("img");
-            img.classList.add("img-fluid", "m-2");
-            img.style.maxWidth = "50%";
-            img.src = imageUrl;
-            img.alt = `Image from ${listData.title}`;
-            imgContainer.append(img);
-          }
-          list.append(imgContainer);
-        } else {
-          const img = document.createElement("img");
-          img.classList.add("img-fluid");
-          img.src = listData.media;
-          img.alt = `Image from ${listData.title}`;
-          list.append(img);
-        }
+  const list = document.createElement("div");
+  list.classList.add(
+      "mainlist",
+      "sm-md-w-100",
+      "lg-w-50",
+      "container-fluid",
+      "d-flex",
+      "flex-column",
+      "border",
+      "border-primary"
+  );
+  list.innerHTML = `<h2 class="text-center mt-5">${listData.title}</h2> <br> <p>${listData.description}</p>`;
+
+  if (listData.media) {
+    if (Array.isArray(listData.media)) {
+      const imgContainer = document.createElement("div");
+      imgContainer.classList.add("d-flex", "flex-column", "justify-content-center", "align-items-center");
+      for (const imageUrl of listData.media) {
+        const img = document.createElement("img");
+        img.classList.add("img-fluid", "m-2");
+        img.style.maxWidth = "50%";
+        img.src = imageUrl;
+        img.alt = `Image from ${listData.title}`;
+        imgContainer.append(img);
       }
-
-    const body = document.createElement("p");
-    body.innerHTML = `Auction ends at: ${listData.endsAt}`;
-    list.append(body);
-
-    if (listData.tags) {
-        const tags = document.createElement("label");
-        tags.innerText = `Tags: ${listData.tags}`;
-        list.append(tags)
+      list.append(imgContainer);
+    } else {
+      const img = document.createElement("img");
+      img.classList.add("img-fluid");
+      img.src = listData.media;
+      img.alt = `Image from ${listData.title}`;
+      list.append(img);
     }
+  }
 
-    const bidTotal = listData.bids.reduce((total, bid) => total + bid.amount, 0);
-      const bid = document.createElement("h4");
-      bid.classList.add("text-center");
-      bid.innerHTML = `Bids Amount $: ${bidTotal}`;
-      list.append(bid);
+  const body = document.createElement("p");
+  body.innerHTML = `Auction ends at: ${listData.endsAt}`;
+  list.append(body);
 
-    const profileName = JSON.parse(window.localStorage.getItem('profile'))
+  if (listData.tags) {
+      const tags = document.createElement("label");
+      tags.innerText = `Tags: ${listData.tags}`;
+      list.append(tags);
+  }
 
-    // console.log(profileName.name);
-    // console.log(listData.seller.name);
+  let bidTotal = listData.bids.reduce((total, bid) => total + bid.amount, 0);
+  const bid = document.createElement("h4");
+  bid.classList.add("text-center");
+  bid.innerHTML = `Bids Amount $: ${bidTotal}`;
+  list.append(bid);
 
-    // if (profileName.name === listData.seller.name) {
-    //         const btn = document.createElement("button");
-    //         // btn.classList.add("btn", "btn-primary", "btn-sm")
-    //         btn.innerHTML =`<a href="edit_list.html?id=${listData.id}" class="text-muted">Edit</a>`;
-    //         list.append(btn);
-            
-            // const deleteBtn = document.createElement("button");
-            // deleteBtn.classList.add("deletebtn");
-            // const removeButton = document.querySelector("deleteBtn");
-	        // removeButton.addEventListener("click", async () => {
-            // await removelist(listData.id)
-            // })
+  const profileInfo = JSON.parse(window.localStorage.getItem("profile"));
 
-        // } else {
-        //     const btn = document.createElement("button");
-        //     btn.classList.add("btn")
-        //     btn.innerHTML =`<a href="profile.html">Back to lists</a>`;
-        //     list.append(btn);;
-        // }
-
-    return list;
+  if (profileInfo.name !== listData.seller.name) {
+    const form = document.createElement("form");
+    form.innerHTML = `
+      <div class="form-group">
+        <label for="bidAmount">Bid Amount:</label>
+        <input type="number" class="form-control" id="bidAmount" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Place Bid</button>
+    `;
+  
+    const error = document.createElement("p");
+    error.classList.add("text-danger");
+  
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      let bidAmount = parseInt(form.querySelector("#bidAmount").value);
+      if (bidAmount > bidTotal && bidAmount <= profileInfo.credits) {
+        const newBid = {
+          amount: bidAmount,
+          bidder: profileInfo.name,
+        };
+        listData.bids.push(newBid);
+        const response = await tokenFetch(`${API_LISTINGS_URL}/${listData.id}/bids`, {
+          method: "POST",
+          body: JSON.stringify(newBid),
+        });
+        if (response.ok) {
+          profileInfo.credits -= bidAmount;
+          window.localStorage.setItem("profile", JSON.stringify(profileInfo));
+          bidTotal = bidAmount;
+          bid.innerHTML = `Bids Amount $: ${bidTotal}`;
+          form.querySelector("#bidAmount").value = "";
+          error.innerText = "";
+        }
+      } else {
+        error.innerText = "Bid amount must be greater than the current bid total";
+      }
+    });
+  
+    form.append(error);
+    list.append(form);
+  }
+  
+  return list;
 }
+
 
 export function renderLists(listDataList, parent) {
     const listHTMLElements = listDataList.map(listTemplate);
