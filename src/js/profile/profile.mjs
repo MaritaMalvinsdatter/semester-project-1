99c8ebb96c272f274f616bb5062d9083331a487f
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-02-28 18:18:32 +0100

    updated avatar saved to server

--- src/js/profile/profile.mjs
+++ src/js/profile/profile.mjs
@@ -1,27 +1,28 @@
 import { load, save } from "../api/tokenStorage.mjs";
+import { headers } from "../api/tokenFetch.mjs";
+import { API_AUCTION_PROFILE } from "../api/constants.mjs";
+
 
 
 // Fetch user info and displays on profile page
+// Fetch user info and displays on profile page
 export function setProfilePage() {
-    const profile = load("profile");
-    // console.log("Profile data:", profile);
-    const nameElem = document.querySelector("#profile-name");
-    const emailElem = document.querySelector("#profile-email");
-    const creditElem = document.querySelector("#profile-credit");
-    const avatarElem = document.querySelector("#profile-avatar");
+  const profile = load("profile");
+  const nameElem = document.querySelector("#profile-name");
+  const emailElem = document.querySelector("#profile-email");
+  const creditElem = document.querySelector("#profile-credit");
+  const avatarElem = document.querySelector("#profile-avatar");
 
-    nameElem.textContent = profile.name;
-    emailElem.textContent = profile.email;
-    creditElem.textContent = "Credit: " + profile.credits;
+  nameElem.textContent = profile.name;
+  emailElem.textContent = profile.email;
+  creditElem.textContent = "Credit: " + profile.credits;
 
-    if (profile.avatar) {
-        avatarElem.src = profile.avatar;
-    } else if (profile.avatar === "null") {
-      delete profile.avatar;
-    } else {
-      delete profile.avatar;
-    }
-    // console.log("Profile data after setting avatar:", profile);
+  if (profile.avatar) {
+    avatarElem.src = profile.avatar;
+  } else {
+    avatarElem.src = "/src/img/blank-profile-picture-gca82a1260_640.png";
+    save("profile", profile); // save updated profile to local storage
+  }
 }
 
 // Updates avatar
@@ -42,15 +43,28 @@ export function updateAvatar() {
         editAvatarBtn.hidden = true;
       });
       
-      updateAvatarBtn.addEventListener("click", () => {
+      updateAvatarBtn.addEventListener("click", async () => {
         const newAvatar = avatarInput.value;
-        profile.avatar = newAvatar;
-        save("profile", profile);
-        setProfilePage(); 
-        avatarElem.src = newAvatar;
-        avatarInput.hidden = true;
-        updateAvatarBtn.hidden = true;
-        editAvatarBtn.hidden = false;
+        try {
+          const response = await fetch(`${API_AUCTION_PROFILE}${profile.name}/media`, {
+            method: "PUT",
+            headers: headers(),
+            body: JSON.stringify({ avatar: newAvatar })
+          });
+          if (!response.ok) {
+            throw new Error("Failed to update avatar.");
+          }
+          const updatedProfile = await response.json();
+          profile.avatar = updatedProfile.avatar;
+          save("profile", profile);
+          avatarElem.src = updatedProfile.avatar;
+          avatarInput.hidden = true;
+          updateAvatarBtn.hidden = true;
+          editAvatarBtn.hidden = false;
+        } catch (error) {
+          console.error(error);
+          alert("Failed");
+        }
       });
 }
 
