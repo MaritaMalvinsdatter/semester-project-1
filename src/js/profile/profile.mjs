d2811ae37063804cecbc2bbf0ee70dfac22b1d13
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-03-01 08:20:37 +0100

    error warning if empty input

--- src/js/profile/profile.mjs
+++ src/js/profile/profile.mjs
@@ -27,46 +27,63 @@ export function setProfilePage() {
 
 // Updates avatar
 const profile = load("profile");
-
 const avatarElem = document.getElementById("profile-avatar");
 const editAvatarBtn = document.getElementById("edit-avatar-btn");
 const avatarInput = document.getElementById("avatar-input");
 const updateAvatarBtn = document.getElementById("update-avatar-btn");
+const userAlert = document.querySelector("#error-alert");
+
 
 export function updateAvatar() {
-
   avatarElem.src = profile.avatar;
-  
-    editAvatarBtn.addEventListener("click", () => {
-        avatarInput.hidden = false;
-        updateAvatarBtn.hidden = false;
-        editAvatarBtn.hidden = true;
+
+  editAvatarBtn.addEventListener("click", () => {
+    avatarInput.hidden = false;
+    updateAvatarBtn.hidden = false;
+    editAvatarBtn.hidden = true;
+  });
+
+  avatarInput.addEventListener("input", () => {
+    userAlert.innerHTML = "";
+  });
+
+  userAlert.addEventListener("click", () => {
+    userAlert.innerHTML = "";
+  });
+
+  updateAvatarBtn.addEventListener("click", async () => {
+    const newAvatar = avatarInput.value;
+    if (!newAvatar) {
+      userAlert.classList.add("alert-warning");
+      userAlert.innerHTML = `A valid image URL is needed to update the avatar`;
+      return;
+    }
+    try {
+      const response = await fetch(`${API_AUCTION_PROFILE}${profile.name}/media`, {
+        method: "PUT",
+        headers: headers(),
+        body: JSON.stringify({ avatar: newAvatar })
       });
-      
-      updateAvatarBtn.addEventListener("click", async () => {
-        const newAvatar = avatarInput.value;
-        try {
-          const response = await fetch(`${API_AUCTION_PROFILE}${profile.name}/media`, {
-            method: "PUT",
-            headers: headers(),
-            body: JSON.stringify({ avatar: newAvatar })
-          });
-          if (!response.ok) {
-            throw new Error("Failed to update avatar.");
-          }
-          const updatedProfile = await response.json();
-          profile.avatar = updatedProfile.avatar;
-          save("profile", profile);
-          avatarElem.src = updatedProfile.avatar;
-          avatarInput.hidden = true;
-          updateAvatarBtn.hidden = true;
-          editAvatarBtn.hidden = false;
-        } catch (error) {
-          console.error(error);
-          alert("Failed");
-        }
-      });
+      if (!response.ok) {
+        throw new Error("Failed to update avatar.");
+      }
+      const updatedProfile = await response.json();
+      profile.avatar = updatedProfile.avatar;
+      save("profile", profile);
+      avatarElem.src = updatedProfile.avatar;
+      avatarInput.hidden = true;
+      updateAvatarBtn.hidden = true;
+      editAvatarBtn.hidden = false;
+      userAlert.hidden = true;
+    } catch (error) {
+      console.error(error);
+      alert("Failed");
+    }
+  });
 }
+
+
+
 
 
 
