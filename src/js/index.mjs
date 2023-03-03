183d7ffd3f2fc82442dbf430f6e459e60f47c958
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-03-03 15:40:15 +0100

    removed unused code and console.log

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -11,44 +11,7 @@ import { setRemoveListingListener } from "./listings/deleteListing.mjs";
 import { setEditListingListener } from "./listings/editListing.mjs"
 import { setLogoutListener } from "./login_register/logout.mjs";
 
-// console.log(paths.API_SELLER_URL)
-
 const path = location.pathname
-
-// switch (path) {
-//     case "/":
-//       break;
-//     case "/login/index.html":
-//       setLoginFormListener();
-//       setLogoutListener();
-//       break;
-//     case "/register/index.html":
-//       setRegisterFormListener();
-//       setLogoutListener();
-//       break;
-//     case "/index.html":
-//       getListings();
-//       setNewListingListener();
-//       setLogoutListener();
-//       viewMoreButton();
-//       break;
-//     case "/listingItem/index.html":
-//       getOneListing();
-//       setLogoutListener();
-//       setEditListingListener();
-//       setRemoveListingListener();
-//       break;
-//     case "/profile/index.html":
-//       setProfilePage();
-//       updateAvatar();
-//       getOwnListingings()
-//       setLogoutListener();
-//       break;
-//     default:
-//       setLogoutListener();
-//       break;
-//   } 
-
   switch (path) {
     case "/":
     case "/index.html":
@@ -83,27 +46,8 @@ const path = location.pathname
       break;
   }
 
-  console.log(path)
 
-// switch (path) {
-//     case "/":
-//     case "/index.html":
-//         getListings();
-//         setLogoutListener();
-//         setNewListingListener();
-//         break;
-//     case "/login/":
-//     case "/login/index.html":
-//         setLoginFormListener();
-//         break;
-//     case "/register/":
-//     case "/register/index.html":
-//         setRegisterFormListener();
-//         break; 
-//     case "/listingItem/index.html":
-//         getOneListing();
-//         break;       
-// }
+
 
 
 
